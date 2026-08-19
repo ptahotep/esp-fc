26df812094a87fcf8bad2a36dda32f673c4ef46a
ptahotep <flyghtmind@gmail.com>
2026-08-20 00:57:24 +0300

    Update BusI2C.cpp

--- lib/Espfc/src/Device/BusI2C.cpp
+++ lib/Espfc/src/Device/BusI2C.cpp
@@ -18,6 +18,11 @@ int BusI2C::begin(int sda, int scl, uint32_t speed)
 {
   if (sda == -1 || scl == -1) return 0;
 
+  // Если передано больше 400 кГц (например 800000), урезаем до 400000
+  if (speed > 400000) {
+    speed = 400000;
+  }
+
   targetI2CInit(_dev, sda, scl, speed);
 
   return 1;
